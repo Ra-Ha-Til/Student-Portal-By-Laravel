089647c5592513dd1f29b203d4495bb3ea36c923
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-06 22:44:49 +0600

    all table migrated

--- database/migrations/2025_07_06_164237_create_enrollments_table.php
+++ database/migrations/2025_07_06_164237_create_enrollments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('enrollments', function (Blueprint $table) {
             $table->id();
+            $table->string('enroll_no')->unique();
+            $table->foreignId('batch_id')->constrained();
+            $table->foreignId('student_id')->constrained();
+            $table->date('join_date');
+            $table->decimal('fee', 10, 2);
             $table->timestamps();
         });
     }
--- database/migrations/2025_07_06_164237_create_payments_table.php
+++ database/migrations/2025_07_06_164237_create_payments_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,9 @@ return new class extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('enrollment_id')->constrained();
+            $table->date('paid_date');
+            $table->decimal('amount', 10, 2);
             $table->timestamps();
         });
     }
