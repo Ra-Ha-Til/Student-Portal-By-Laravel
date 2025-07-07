acb29c9e3145dcb3200bbc044ec4255adf32cf2d
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-07 10:26:49 +0600

    route set

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,27 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Student;
+use App\Models\Teacher;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
-        return view('home');
+        $studentCount = Student::count();
+        $teacherCount = Teacher::count();
+
+        return view('dashboard', [
+            'studentCount' => $studentCount,
+            'teacherCount' => $teacherCount,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\TeacherController;
 use App\Http\Controllers\CourseController;
@@ -9,17 +9,13 @@ use App\Http\Controllers\BatchController;
 use App\Http\Controllers\EnrollmentController;
 use App\Http\Controllers\PaymentController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-*/
-
 Route::get('/', function () {
     return redirect('/dashboard');
 });
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+Auth::routes();
+
+Route::get('/dashboard', [HomeController::class, 'index'])->name('dashboard');
 
 Route::resources([
     'students' => StudentController::class,
@@ -29,6 +25,3 @@ Route::resources([
     'enrollments' => EnrollmentController::class,
     'payments' => PaymentController::class,
 ]);
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
