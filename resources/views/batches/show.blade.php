6ac6f922b09d11ebd82814b1d063c2bcd1acfdd6
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:43:08 +0600

    show created for batch

--- resources/views/batches/show.blade.php
+++ resources/views/batches/show.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 
-@section('title', 'View Student')
+@section('title', 'View Batch')
 
 @section('content')
 <div class="card">
-    <div class="card-header">Student Details</div>
+    <div class="card-header">Batch Details</div>
     <div class="card-body">
         <div class="card-body">
-            <h5 class="card-title">Name: {{ $student->name }}</h5>
-            <p class="card-text">Address: {{ $student->address }}</p>
-            <p class="card-text">Mobile: {{ $student->mobile }}</p>
+            <h5 class="card-title">Name: {{ $batch->name }}</h5>
+            <p class="card-text">Course: {{ $batch->course->name }}</p>
+            <p class="card-text">Start Date: {{ $batch->start_date }}</p>
         </div>
         </hr>
     </div>
