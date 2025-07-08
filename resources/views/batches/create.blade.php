a1994b2a757bbb82e14b2b089a9576377529da8e
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:41:38 +0600

    create for batch

--- resources/views/batches/create.blade.php
+++ resources/views/batches/create.blade.php
@@ -1,19 +1,23 @@
 @extends('layouts.app')
 
-@section('title', 'Add Student')
+@section('title', 'Add Batch')
 
 @section('content')
 <div class="card">
-    <div class="card-header">Students Page</div>
+    <div class="card-header">Batches</div>
     <div class="card-body">
-        <form action="{{ route('students.store') }}" method="POST">
+        <form action="{{ route('batches.store') }}" method="POST">
             @csrf
-            <label>Name</label><br>
+            <label>Batch Name</label><br>
             <input type="text" name="name" id="name" class="form-control"><br>
-            <label>Address</label><br>
-            <input type="text" name="address" id="address" class="form-control"><br>
-            <label>Mobile</label><br>
-            <input type="text" name="mobile" id="mobile" class="form-control"><br>
+            <label>Course</label><br>
+            <select name="course_id" id="course_id" class="form-control">
+                @foreach($courses as $id => $name)
+                <option value="{{ $id }}">{{ $name }}</option>
+                @endforeach
+            </select><br>
+            <label>Start Date</label><br>
+            <input type="date" name="start_date" id="start_date" class="form-control"><br>
             <input type="submit" value="Save" class="btn btn-success"><br>
         </form>
     </div>
