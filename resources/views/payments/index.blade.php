03a21b8a77e50b493d28599e8fc50c771592ef95
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:50:29 +0600

    index created for payment

--- resources/views/payments/index.blade.php
+++ resources/views/payments/index.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
-@section('title', 'Batches')
+@section('title', 'Payments')
 
 @section('content')
 <div class="card">
     <div class="card-header">
-        <h2>Batches</h2>
+        <h2>Payment List</h2>
     </div>
     <div class="card-body">
-        <a href="{{ route('batches.create') }}" class="btn btn-success btn-sm" title="Add New Batch">
+        <a href="{{ route('payments.create') }}" class="btn btn-success btn-sm" title="Add New Payment">
             <i class="fa fa-plus" aria-hidden="true"></i> Add New
         </a>
         <br><br>
@@ -17,35 +17,35 @@
                 <thead>
                     <tr>
                         <th>#</th>
-                        <th>Name</th>
-                        <th>Course</th>
-                        <th>Start Date</th>
+                        <th>Enrollment No</th>
+                        <th>Paid Date</th>
+                        <th>Amount</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($batches as $batch)
+                    @foreach($payments as $payment)
                     <tr>
                         <td>{{ $loop->iteration }}</td>
-                        <td>{{ $batch->name }}</td>
-                        <td>{{ $batch->course->name }}</td>
-                        <td>{{ $batch->start_date }}</td>
+                        <td>{{ $payment->enrollment->enroll_no }}</td>
+                        <td>{{ $payment->paid_date }}</td>
+                        <td>{{ $payment->amount }}</td>
                         <td>
-                            <a href="{{ route('batches.show', $batch->id) }}" title="View Batch">
+                            <a href="{{ route('payments.show', $payment->id) }}" title="View Payment">
                                 <button class="btn btn-info btn-sm">
                                     <i class="fa fa-eye" aria-hidden="true"></i> View
                                 </button>
                             </a>
-                            <a href="{{ route('batches.edit', $batch->id) }}" title="Edit Batch">
+                            <a href="{{ route('payments.edit', $payment->id) }}" title="Edit Payment">
                                 <button class="btn btn-primary btn-sm">
                                     <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
                                 </button>
                             </a>
-                            <form method="POST" action="{{ route('batches.destroy', $batch->id) }}"
+                            <form method="POST" action="{{ route('payments.destroy', $payment->id) }}"
                                 accept-charset="UTF-8" style="display:inline">
                                 @method('DELETE')
                                 @csrf
-                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Batch"
+                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Payment"
                                     onclick="return confirm('Confirm delete?')">
                                     <i class="fa fa-trash-o" aria-hidden="true"></i> Delete
                                 </button>
