5af3ea93dbe3d699258442d650bd90a1c73abc9c
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:50:57 +0600

    create created for payment

--- resources/views/payments/create.blade.php
+++ resources/views/payments/create.blade.php
@@ -1,23 +1,23 @@
 @extends('layouts.app')
 
-@section('title', 'Add Batch')
+@section('title', 'Add Payment')
 
 @section('content')
 <div class="card">
-    <div class="card-header">Batches</div>
+    <div class="card-header">Payment</div>
     <div class="card-body">
-        <form action="{{ route('batches.store') }}" method="POST">
+        <form action="{{ route('payments.store') }}" method="POST">
             @csrf
-            <label>Batch Name</label><br>
-            <input type="text" name="name" id="name" class="form-control"><br>
-            <label>Course</label><br>
-            <select name="course_id" id="course_id" class="form-control">
-                @foreach($courses as $id => $name)
-                <option value="{{ $id }}">{{ $name }}</option>
+            <label>Enrollment</label><br>
+            <select name="enrollment_id" id="enrollment_id" class="form-control">
+                @foreach($enrollments as $id => $enroll_no)
+                <option value="{{ $id }}">{{ $enroll_no }}</option>
                 @endforeach
             </select><br>
-            <label>Start Date</label><br>
-            <input type="date" name="start_date" id="start_date" class="form-control"><br>
+            <label>Paid Date</label><br>
+            <input type="date" name="paid_date" id="paid_date" class="form-control"><br>
+            <label>Amount</label><br>
+            <input type="number" name="amount" id="amount" class="form-control" step="0.01"><br>
             <input type="submit" value="Save" class="btn btn-success"><br>
         </form>
     </div>
