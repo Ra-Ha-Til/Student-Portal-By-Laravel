8fbe0c713e240b2c5ee5cee54164c4bce8942809
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:51:48 +0600

    show

--- resources/views/payments/show.blade.php
+++ resources/views/payments/show.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 
-@section('title', 'View Batch')
+@section('title', 'View Payment')
 
 @section('content')
 <div class="card">
-    <div class="card-header">Batch Details</div>
+    <div class="card-header">Payment Details</div>
     <div class="card-body">
         <div class="card-body">
-            <h5 class="card-title">Name: {{ $batch->name }}</h5>
-            <p class="card-text">Course: {{ $batch->course->name }}</p>
-            <p class="card-text">Start Date: {{ $batch->start_date }}</p>
+            <h5 class="card-title">Enrollment No: {{ $payment->enrollment->enroll_no }}</h5>
+            <p class="card-text">Paid Date: {{ $payment->paid_date }}</p>
+            <p class="card-text">Amount: {{ $payment->amount }}</p>
         </div>
         </hr>
     </div>
