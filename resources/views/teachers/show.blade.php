8ee4273017b53f60bad6be05cfce34e61279cb1e
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:28:28 +0600

    teacher portal created

---
+++ resources/views/teachers/show.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('title', 'View Student')
+
+@section('content')
+<div class="card">
+    <div class="card-header">Student Details</div>
+    <div class="card-body">
+        <div class="card-body">
+            <h5 class="card-title">Name: {{ $student->name }}</h5>
+            <p class="card-text">Address: {{ $student->address }}</p>
+            <p class="card-text">Mobile: {{ $student->mobile }}</p>
+        </div>
+        </hr>
+    </div>
+</div>
+@endsection
