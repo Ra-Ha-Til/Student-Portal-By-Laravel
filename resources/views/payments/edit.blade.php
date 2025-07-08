b3f39c53bdfc5d1ef29c19fa7783f8922cfcb8e6
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:51:24 +0600

    edit  created for payments

--- resources/views/payments/edit.blade.php
+++ resources/views/payments/edit.blade.php
@@ -1,25 +1,26 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Batch')
+@section('title', 'Edit Payment')
 
 @section('content')
 <div class="card">
-    <div class="card-header">Edit Batch</div>
+    <div class="card-header">Edit Payment</div>
     <div class="card-body">
-        <form action="{{ route('batches.update', $batch->id) }}" method="POST">
+        <form action="{{ route('payments.update', $payment->id) }}" method="POST">
             @csrf
             @method('PUT')
-            <input type="hidden" name="id" value="{{ $batch->id }}">
-            <label>Batch Name</label><br>
-            <input type="text" name="name" value="{{ $batch->name }}" class="form-control"><br>
-            <label>Course</label><br>
-            <select name="course_id" id="course_id" class="form-control">
-                @foreach($courses as $id => $name)
-                <option value="{{ $id }}" {{ $batch->course_id == $id ? 'selected' : '' }}>{{ $name }}</option>
+            <input type="hidden" name="id" value="{{ $payment->id }}">
+            <label>Enrollment</label><br>
+            <select name="enrollment_id" id="enrollment_id" class="form-control">
+                @foreach($enrollments as $id => $enroll_no)
+                <option value="{{ $id }}" {{ $payment->enrollment_id == $id ? 'selected' : '' }}>{{ $enroll_no }}
+                </option>
                 @endforeach
             </select><br>
-            <label>Start Date</label><br>
-            <input type="date" name="start_date" value="{{ $batch->start_date }}" class="form-control"><br>
+            <label>Paid Date</label><br>
+            <input type="date" name="paid_date" value="{{ $payment->paid_date }}" class="form-control"><br>
+            <label>Amount</label><br>
+            <input type="number" name="amount" value="{{ $payment->amount }}" class="form-control" step="0.01"><br>
             <input type="submit" value="Update" class="btn btn-success"><br>
         </form>
     </div>
