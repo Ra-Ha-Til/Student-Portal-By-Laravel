154ca149e83f2a010a516c4e0c64ffe64b0619ce
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:47:17 +0600

    eidt created for enrollment

--- resources/views/enrollments/edit.blade.php
+++ resources/views/enrollments/edit.blade.php
@@ -1,25 +1,36 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Batch')
+@section('title', 'Edit Enrollment')
 
 @section('content')
 <div class="card">
-    <div class="card-header">Edit Batch</div>
+    <div class="card-header">Edit Enrollment</div>
     <div class="card-body">
-        <form action="{{ route('batches.update', $batch->id) }}" method="POST">
+        <form action="{{ route('enrollments.update', $enrollment->id) }}" method="POST">
             @csrf
             @method('PUT')
-            <input type="hidden" name="id" value="{{ $batch->id }}">
-            <label>Batch Name</label><br>
-            <input type="text" name="name" value="{{ $batch->name }}" class="form-control"><br>
-            <label>Course</label><br>
-            <select name="course_id" id="course_id" class="form-control">
-                @foreach($courses as $id => $name)
-                <option value="{{ $id }}" {{ $batch->course_id == $id ? 'selected' : '' }}>{{ $name }}</option>
+            <input type="hidden" name="id" value="{{ $enrollment->id }}">
+            <label>Enroll No</label><br>
+            <input type="text" name="enroll_no" id="enroll_no" class="form-control"
+                value="{{ $enrollment->enroll_no }}"><br>
+            <label>Batch</label><br>
+            <select name="batch_id" id="batch_id" class="form-control">
+                @foreach($batches as $id => $name)
+                <option value="{{ $id }}" {{ $enrollment->batch_id == $id ? 'selected' : '' }}>{{ $name }}</option>
                 @endforeach
             </select><br>
-            <label>Start Date</label><br>
-            <input type="date" name="start_date" value="{{ $batch->start_date }}" class="form-control"><br>
+            <label>Student</label><br>
+            <select name="student_id" id="student_id" class="form-control">
+                @foreach($students as $id => $name)
+                <option value="{{ $id }}" {{ $enrollment->student_id == $id ? 'selected' : '' }}>{{ $name }}</option>
+                @endforeach
+            </select><br>
+            <label>Join Date</label><br>
+            <input type="date" name="join_date" id="join_date" class="form-control"
+                value="{{ $enrollment->join_date }}"><br>
+            <label>Fee</label><br>
+            <input type="number" name="fee" id="fee" class="form-control" step="0.01"
+                value="{{ $enrollment->fee }}"><br>
             <input type="submit" value="Update" class="btn btn-success"><br>
         </form>
     </div>
