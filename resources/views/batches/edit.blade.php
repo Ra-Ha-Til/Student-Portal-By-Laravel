a73e67a44e26ceba06a3d6210c7018cbf059c0ac
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:42:16 +0600

    edit created for batch

--- resources/views/batches/edit.blade.php
+++ resources/views/batches/edit.blade.php
@@ -1,21 +1,25 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Student')
+@section('title', 'Edit Batch')
 
 @section('content')
 <div class="card">
-    <div class="card-header">Edit Student</div>
+    <div class="card-header">Edit Batch</div>
     <div class="card-body">
-        <form action="{{ route('students.update', $student->id) }}" method="POST">
+        <form action="{{ route('batches.update', $batch->id) }}" method="POST">
             @csrf
             @method('PUT')
-            <input type="hidden" name="id" value="{{ $student->id }}">
-            <label>Name</label><br>
-            <input type="text" name="name" value="{{ $student->name }}" class="form-control"><br>
-            <label>Address</label><br>
-            <input type="text" name="address" value="{{ $student->address }}" class="form-control"><br>
-            <label>Mobile</label><br>
-            <input type="text" name="mobile" value="{{ $student->mobile }}" class="form-control"><br>
+            <input type="hidden" name="id" value="{{ $batch->id }}">
+            <label>Batch Name</label><br>
+            <input type="text" name="name" value="{{ $batch->name }}" class="form-control"><br>
+            <label>Course</label><br>
+            <select name="course_id" id="course_id" class="form-control">
+                @foreach($courses as $id => $name)
+                <option value="{{ $id }}" {{ $batch->course_id == $id ? 'selected' : '' }}>{{ $name }}</option>
+                @endforeach
+            </select><br>
+            <label>Start Date</label><br>
+            <input type="date" name="start_date" value="{{ $batch->start_date }}" class="form-control"><br>
             <input type="submit" value="Update" class="btn btn-success"><br>
         </form>
     </div>
