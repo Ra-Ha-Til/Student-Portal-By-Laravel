273d88f28301117b3c6c3b1f9979782729692ddb
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:40:22 +0600

    batches bar created

---
+++ resources/views/batches/edit.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('title', 'Edit Student')
+
+@section('content')
+<div class="card">
+    <div class="card-header">Edit Student</div>
+    <div class="card-body">
+        <form action="{{ route('students.update', $student->id) }}" method="POST">
+            @csrf
+            @method('PUT')
+            <input type="hidden" name="id" value="{{ $student->id }}">
+            <label>Name</label><br>
+            <input type="text" name="name" value="{{ $student->name }}" class="form-control"><br>
+            <label>Address</label><br>
+            <input type="text" name="address" value="{{ $student->address }}" class="form-control"><br>
+            <label>Mobile</label><br>
+            <input type="text" name="mobile" value="{{ $student->mobile }}" class="form-control"><br>
+            <input type="submit" value="Update" class="btn btn-success"><br>
+        </form>
+    </div>
+</div>
+@endsection
