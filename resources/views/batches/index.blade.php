758cfe96051af94a7c57a34c7500700959e38399
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:41:07 +0600

    index created for batches

--- resources/views/batches/index.blade.php
+++ resources/views/batches/index.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
-@section('title', 'Students')
+@section('title', 'Batches')
 
 @section('content')
 <div class="card">
     <div class="card-header">
-        <h2>Student List</h2>
+        <h2>Batches</h2>
     </div>
     <div class="card-body">
-        <a href="{{ route('students.create') }}" class="btn btn-success btn-sm" title="Add New Student">
+        <a href="{{ route('batches.create') }}" class="btn btn-success btn-sm" title="Add New Batch">
             <i class="fa fa-plus" aria-hidden="true"></i> Add New
         </a>
         <br><br>
@@ -18,34 +18,34 @@
                     <tr>
                         <th>#</th>
                         <th>Name</th>
-                        <th>Address</th>
-                        <th>Mobile</th>
+                        <th>Course</th>
+                        <th>Start Date</th>
                         <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    @foreach($students as $student)
+                    @foreach($batches as $batch)
                     <tr>
                         <td>{{ $loop->iteration }}</td>
-                        <td>{{ $student->name }}</td>
-                        <td>{{ $student->address }}</td>
-                        <td>{{ $student->mobile }}</td>
+                        <td>{{ $batch->name }}</td>
+                        <td>{{ $batch->course->name }}</td>
+                        <td>{{ $batch->start_date }}</td>
                         <td>
-                            <a href="{{ route('students.show', $student->id) }}" title="View Student">
+                            <a href="{{ route('batches.show', $batch->id) }}" title="View Batch">
                                 <button class="btn btn-info btn-sm">
                                     <i class="fa fa-eye" aria-hidden="true"></i> View
                                 </button>
                             </a>
-                            <a href="{{ route('students.edit', $student->id) }}" title="Edit Student">
+                            <a href="{{ route('batches.edit', $batch->id) }}" title="Edit Batch">
                                 <button class="btn btn-primary btn-sm">
                                     <i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit
                                 </button>
                             </a>
-                            <form method="POST" action="{{ route('students.destroy', $student->id) }}"
+                            <form method="POST" action="{{ route('batches.destroy', $batch->id) }}"
                                 accept-charset="UTF-8" style="display:inline">
                                 @method('DELETE')
                                 @csrf
-                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Student"
+                                <button type="submit" class="btn btn-danger btn-sm" title="Delete Batch"
                                     onclick="return confirm('Confirm delete?')">
                                     <i class="fa fa-trash-o" aria-hidden="true"></i> Delete
                                 </button>
