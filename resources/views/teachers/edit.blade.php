5e1d1c60dfb0416d9f3d74a0fbc69040d147057b
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:30:42 +0600

    edit blade created for teacher

--- resources/views/teachers/edit.blade.php
+++ resources/views/teachers/edit.blade.php
@@ -1,21 +1,21 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Student')
+@section('title', 'Edit Teacher')
 
 @section('content')
 <div class="card">
-    <div class="card-header">Edit Student</div>
+    <div class="card-header">Edit Teacher</div>
     <div class="card-body">
-        <form action="{{ route('students.update', $student->id) }}" method="POST">
+        <form action="{{ route('teachers.update', $teacher->id) }}" method="POST">
             @csrf
             @method('PUT')
-            <input type="hidden" name="id" value="{{ $student->id }}">
+            <input type="hidden" name="id" value="{{ $teacher->id }}">
             <label>Name</label><br>
-            <input type="text" name="name" value="{{ $student->name }}" class="form-control"><br>
+            <input type="text" name="name" value="{{ $teacher->name }}" class="form-control"><br>
             <label>Address</label><br>
-            <input type="text" name="address" value="{{ $student->address }}" class="form-control"><br>
+            <input type="text" name="address" value="{{ $teacher->address }}" class="form-control"><br>
             <label>Mobile</label><br>
-            <input type="text" name="mobile" value="{{ $student->mobile }}" class="form-control"><br>
+            <input type="text" name="mobile" value="{{ $teacher->mobile }}" class="form-control"><br>
             <input type="submit" value="Update" class="btn btn-success"><br>
         </form>
     </div>
