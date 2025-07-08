8c96d0e1cde5302e248a217fb698805d14d76203
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:31:40 +0600

    show blade created for teacher

--- resources/views/teachers/show.blade.php
+++ resources/views/teachers/show.blade.php
@@ -1,15 +1,15 @@
 @extends('layouts.app')
 
-@section('title', 'View Student')
+@section('title', 'View Teacher')
 
 @section('content')
 <div class="card">
-    <div class="card-header">Student Details</div>
+    <div class="card-header">Teacher Details</div>
     <div class="card-body">
         <div class="card-body">
-            <h5 class="card-title">Name: {{ $student->name }}</h5>
-            <p class="card-text">Address: {{ $student->address }}</p>
-            <p class="card-text">Mobile: {{ $student->mobile }}</p>
+            <h5 class="card-title">Name: {{ $teacher->name }}</h5>
+            <p class="card-text">Address: {{ $teacher->address }}</p>
+            <p class="card-text">Mobile: {{ $teacher->mobile }}</p>
         </div>
         </hr>
     </div>
