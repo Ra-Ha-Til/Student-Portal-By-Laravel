3053406be5bf6466a761a1a9feffc4b6fb337d90
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:46:36 +0600

    create created for enrollment

--- resources/views/enrollments/create.blade.php
+++ resources/views/enrollments/create.blade.php
@@ -1,23 +1,31 @@
 @extends('layouts.app')
 
-@section('title', 'Add Batch')
+@section('title', 'Add Enrollment')
 
 @section('content')
 <div class="card">
-    <div class="card-header">Batches</div>
+    <div class="card-header">Enrollment Page</div>
     <div class="card-body">
-        <form action="{{ route('batches.store') }}" method="POST">
+        <form action="{{ route('enrollments.store') }}" method="POST">
             @csrf
-            <label>Batch Name</label><br>
-            <input type="text" name="name" id="name" class="form-control"><br>
-            <label>Course</label><br>
-            <select name="course_id" id="course_id" class="form-control">
-                @foreach($courses as $id => $name)
+            <label>Enroll No</label><br>
+            <input type="text" name="enroll_no" id="enroll_no" class="form-control"><br>
+            <label>Batch</label><br>
+            <select name="batch_id" id="batch_id" class="form-control">
+                @foreach($batches as $id => $name)
                 <option value="{{ $id }}">{{ $name }}</option>
                 @endforeach
             </select><br>
-            <label>Start Date</label><br>
-            <input type="date" name="start_date" id="start_date" class="form-control"><br>
+            <label>Student</label><br>
+            <select name="student_id" id="student_id" class="form-control">
+                @foreach($students as $id => $name)
+                <option value="{{ $id }}">{{ $name }}</option>
+                @endforeach
+            </select><br>
+            <label>Join Date</label><br>
+            <input type="date" name="join_date" id="join_date" class="form-control"><br>
+            <label>Fee</label><br>
+            <input type="number" name="fee" id="fee" class="form-control" step="0.01"><br>
             <input type="submit" value="Save" class="btn btn-success"><br>
         </form>
     </div>
