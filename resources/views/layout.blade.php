b2ed246c52a45215ad3de7a01aef62fcbd466227
Ra-Ha-Til <rahatil880@gmail.com>
2025-07-08 20:53:50 +0600

    modified

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TechSerm - @yield('title')</title>
+    <title>Tech - @yield('title')</title>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
     <style>
@@ -112,7 +112,7 @@
         <nav class="navbar navbar-expand-md navbar-dark shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    TechSerm Education
+                    Tech Education
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent">
